4e2b1f30bd9b4809bcfa1b4180a343648cd3241b
Peter van der Wal <dev@petervanderwal.nl>
2024-12-02 19:44:40 +0100

    fix: ignore "You gave an answer too recently" as wrong answer
    Error: "You gave an answer too recently" was treated as a wrong answer
    Resolved with: Added an additional check on the Advent of Code message
    
    Resolves issue #1

--- src/Model/AnswerSubmissionResult.php
+++ src/Model/AnswerSubmissionResult.php
@@ -13,10 +13,16 @@ class AnswerSubmissionResult
     ) {
         if (preg_match('/(?<! not )the\s+right\s+answer/i', $this->adventOfCodeResponse)) {
             $this->correctAnswer = true;
-        } elseif (preg_match(
-            '/You\s+don.t\s+seem\s+to\s+be\s+solving\s+the\s+right\s+level/i',
-            $this->adventOfCodeResponse
-        )) {
+        } elseif (
+            preg_match(
+                '/You\s+don.t\s+seem\s+to\s+be\s+solving\s+the\s+right\s+level/i',
+                $this->adventOfCodeResponse
+            )
+            || preg_match(
+                '/You\s+gave\s+an\s+answer\s+too\s+recently/i',
+                $this->adventOfCodeResponse
+            )
+        ) {
             $this->correctAnswer = null;
         } else {
             $this->correctAnswer = false;
